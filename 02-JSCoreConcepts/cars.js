3a26de58d4537000caa16d248297f1acdebf09ae
s-115-13 <s-115-13@gumiforest.ae.krakow.pl>
2020-03-03 15:52:56 +0100

    zad4

--- 02-JSCoreConcepts/cars.js
+++ 02-JSCoreConcepts/cars.js
@@ -1,10 +1,14 @@
-function isCar(car1, car2, car3) {
-    // return 
-}
-
-console.log(isCar(true, true, true));
-console.log(isCar(false, false, false));
-console.log(isCar(true, true, false));
-console.log(isCar(false, false, true));
-console.log(isCar(true, false, true));
-
+function isCar(car1, car2, car3) { 
+    if ((car1 && car2 && car3) || (!car1 && !car2 && !car3)) 
+        return true;
+    else 
+        return false
+  }
+  
+  console.log(isCar(true, true, true));
+  console.log(isCar(false, false, false));
+  console.log(isCar(true, true, false));
+  console.log(isCar(false, false, true));
+  console.log(isCar(true, false, true));
+  
+  
