b3f144a16874ce3bdc9e4df93718e9cd7e6287bc
s-115-13 <s-115-13@gumiforest.ae.krakow.pl>
2020-03-03 15:43:47 +0100

    zad2

--- 02-JSCoreConcepts/heron.js
+++ 02-JSCoreConcepts/heron.js
@@ -1,11 +1,12 @@
-// boki trójkąta
-//a = 3;
-b = 4;
-c = 5;
-h = 7
+let a = 3;
+let b = 4;
+let c = 5;
+let h = 7
 
-// Pole trókąta o bokach ..., ... oraz ... wynosi ... .
-
-let a = () => "aaaa";
-
-console.log( a() );
+let result = (a,b,c,h) => 
+  { 
+    let p=(a+b+c)/2;
+    return Math.sqrt(p*(p-a)*(p-b)*(p-c)); 
+  };
+    
+console.log(result(a,b,c,h));
