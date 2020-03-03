88a0cf19face9127587f864aa53dd91fef1eec85
s-115-13 <s-115-13@gumiforest.ae.krakow.pl>
2020-03-03 16:07:52 +0100

    zad5

--- 02-JSCoreConcepts/ordering.js
+++ 02-JSCoreConcepts/ordering.js
@@ -1,25 +1,29 @@
 const reverse = function(arr) {
-    arrReversed = [];
-    arr.forEach(item => arrReversed.push(item));
-    arrReversed.reverse();
-    return arrReversed;
-}
-
-const sort = function(arr) {
-    arrSorted = [];
-    // ...
-    return arrSorted;
-}
-
-const even = function(arr) {
+    let arr2 = [];
+    arr.forEach(x=> arr2.push(x))
+    return arr2.reverse();
+  }
+  
+  const sort = function(arr){
+    let arr2 = [];
+    arr.forEach(x=> arr2.push(x))
+    return arr2.sort()
+  }
+  
+  const even = function(arr) {
     let arrEven = [];
-    // ...
+    for (let i = 0; i < arr.length; i++){
+      if (arr[i] % 2 === 0){
+        arrEven.push(arr[i]);
+      }
+    }
     return arrEven;
-}
-
-numbers = [8,3,2,1,5,4,6];
-console.log(numbers);
-console.log(reverse(numbers));
-console.log(even(numbers));
-console.log(reverse(even(numbers)));
-console.log(even(sort(numbers)));
+  }
+  
+  let numbers = [8,3,2,1,5,4,6];
+  console.log(numbers);
+  console.log(reverse(numbers));
+  console.log(even(numbers));
+  console.log(reverse(even(numbers)));
+  console.log(even(sort(numbers)));
+  
