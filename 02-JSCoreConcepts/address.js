8101febe08008a8b87372dc8b6218c1176602b00
s-115-13 <s-115-13@gumiforest.ae.krakow.pl>
2020-03-03 15:38:01 +0100

    zad1

--- 02-JSCoreConcepts/address.js
+++ 02-JSCoreConcepts/address.js
@@ -1,15 +1,11 @@
-title = "dr";
-name = "Mateusz";
-surname = 'Kubicki';
-street = 'Długa 15';
-city = 'Kraków';
-zip = '30-781';
-country = 'Poland';
+const title = "dr";
+const name = "Mateusz";
+const surname = 'Kubicki';
+const street = 'ul. Długa 15';
+const city = 'Kraków';
+const zip = '30-781';
+const country = 'Poland';
 
 
-/* 
-dr Mateusz Kubicki
-ul. Długa 15
-30-781 Kraków
-POLAND
-*/
+console.log(title + ' ' + name  + ' ' + surname + '\n' + street + '\n' + zip + ' ' + city + '\n' + country);
+console.log(`${title} ${name} ${surname}\n${street}\n${zip} ${city}\n${country}`)
